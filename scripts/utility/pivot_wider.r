494cc7dfb166bcbf92812746546327c5bd931824
Jannes <jannesbr@pik-potsdam.de>
2022-10-14 15:17:39 +0200

    pft_categories solution for both classify_biomes and GAMMA, plus script to be used to create function for tidying data

---
+++ scripts/utility/pivot_wider.r
@@ -0,0 +1,35 @@
+library(tidyverse)
+
+data_file <-"/p/projects/open/Jannes/repos/boundaries/inst/extdata/pft_categories.csv" # nolint
+
+# tidy solution for classify_biomes (can be done analogously for GAMMA)
+pft_table <- readr::read_delim(data_file,
+                               escape_backslash = TRUE,
+                               delim = ",") %>%
+  # change 1, 0.5, 0 values to TRUE and NAs (NA's can be dropped)
+  dplyr::mutate_at(dplyr::vars(dplyr::starts_with(c("category_", "zone_"))),
+                   function(x) ifelse(as.logical(x), TRUE, NA)) %>%
+  # filter natural pfts
+  dplyr::filter(category_natural) %>%
+  # all binary zone columns (tropical, temperate, boreal) in one categorical
+  #   zone column
+  tidyr::pivot_longer(cols = starts_with("zone_"),
+               names_to = "zone",
+               names_prefix = "zone_",
+               values_to = "zone_value",
+               values_drop_na = TRUE) %>%
+  # all binary category columns (natural, needle, evergreen) in one categorical
+  #   category column
+  tidyr::pivot_longer(cols = starts_with("category_"),
+               names_to = "category",
+               names_prefix = "category_",
+               values_to = "category_value",
+               values_drop_na = TRUE) %>%
+  # delete side product - logical columns
+  select(-c("category_value", "zone_value")) %>%
+  # values to lpjml_index, names to length of npft (convert to numeric)
+  tidyr::pivot_longer(cols = starts_with("lpjml_index_npft_"),
+               values_to = "lpjml_index",
+               names_to = "npft_proxy",
+               names_transform = list(npft_proxy = function(x) suppressWarnings(as.numeric(x))), # nolint
+               names_prefix = "lpjml_index_npft_")
