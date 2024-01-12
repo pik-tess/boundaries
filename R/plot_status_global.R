2179b2e1864e41fda847200de1ecf340833fe016
jobraun <jobraun@pik-potsdam.de>
2024-01-12 12:06:33 +0100

    improve global plotting of control variables

--- R/plot_status_global.R
+++ R/plot_status_global.R
@@ -137,74 +137,111 @@ plot_status_global <- function(
                                                     fill = NA, size = 0.5)) +
          ggplot2::theme(axis.title = element_blank())
 
-    
+
   } else {
   # create dataframe for plotting of pb specific background filling
-    pb_thresh <- highrisk_thresh <- holocene <- ylabel <-
-      numeric(length(x))
-    for (i in seq_len(length(x))) {
-      pb_thresh[i] <-  as.numeric(attr(x[[i]], "thresholds")[["pb"]])
-      highrisk_thresh[i] <- as.numeric(
-                              attr(x[[i]], "thresholds")[["highrisk"]]
-                            )
-      holocene[i] <- as.numeric(
-                       attr(x[[i]], "thresholds")[["holocene"]]
-                     )
-      ylabel[i] <-  attr(x[[i]], "control variable")
-      names(ylabel)[i] <- names(x)[i]
-    }
+  pb_thresh <- highrisk_thresh <- holocene <- ylabel <-
+    numeric(length(x))
+  for (i in seq_len(length(x))) {
+    pb_thresh[i] <-  as.numeric(attr(x[[i]], "thresholds")[["pb"]])
+    highrisk_thresh[i] <- as.numeric(
+                            attr(x[[i]], "thresholds")[["highrisk"]]
+                          )
+    holocene[i] <- as.numeric(
+                     attr(x[[i]], "thresholds")[["holocene"]]
+                   )
+    ylabel[i] <-  attr(x[[i]], "control variable")
+    names(ylabel)[i] <- names(x)[i]
+  }
+  df_bg <- data.frame(
+             pb = unique(data_tibble$pb),
+             pb_thresh = pb_thresh,
+             highrisk_thresh = highrisk_thresh,
+             holocene = holocene,
+             years = data_tibble$years[1:length(unique(data_tibble$pb))],
+             values = data_tibble$values[1:length(unique(data_tibble$pb))]
+           )
+  min_years <- min(data_tibble$years)
+  max_years <- max(data_tibble$years)
+  darkgreen <- rgb(84. / 255., 156. / 255., 3. / 255., 1) #
+  green <- rgb(134. / 255., 189. / 255., 36. / 255., 1)
+  lightred <- rgb(244. / 255., 157. / 255., 136. / 255., 1)
+  red <- rgb(197. / 255., 18. / 255., 22. / 255., 1)
+  yellow <- rgb(251. / 255., 206. / 255., 0. / 255., 1)
+  purple <- rgb(117. / 255., 24. / 255., 81. / 255., 1)
 
-    df_bg <- data.frame(
-               pb = unique(data_tibble$pb),
-               pb_thresh = pb_thresh,
-               highrisk_thresh = highrisk_thresh,
-               holocene = holocene,
-               years = data_tibble$years[1:length(unique(data_tibble$pb))],
-               values = data_tibble$values[1:length(unique(data_tibble$pb))]
-             )
-    # plot background
-    plot <- ggplot2::ggplot() +
-      ggplot2::geom_rect(data = df_bg, ggplot2::aes(
-                                        xmin = -Inf, xmax = +Inf,
-                                        ymin = holocene, ymax = pb_thresh),
-                         fill = "#e3f5ec", alpha = 0.8) +
-      ggplot2::geom_rect(data = df_bg, ggplot2::aes(xmin = -Inf, xmax = +Inf,
-                                        ymin = pb_thresh,
-                                        ymax = highrisk_thresh),
-                         fill = "#fcf8d8", alpha = 0.8) +
-      ggplot2::geom_rect(data = df_bg, ggplot2::aes(
-                                            xmin = -Inf, xmax = +Inf,
-                                            ymin = highrisk_thresh,
-                                            ymax = +Inf),
-                           fill = "#f7e4dd", alpha = 0.8) +
-      ggplot2::geom_hline(data = df_bg,
-                          mapping = ggplot2::aes(yintercept = pb_thresh),
-                          linetype = 2, col = "grey") +
-      ggplot2::scale_y_continuous(expand = c(0, NA))
-     #ggplot2::coord_cartesian(ylim = c(0, pb_thresh + 3 * (holocene - pb_thresh)))
-    
-    # plot pb status timeseries
-    #TODO PB itself should be at the same position in all plots
-    plot <- plot +
-         ggplot2::geom_line(data = data_tibble,
-                            ggplot2::aes(x = years, y = values, group = pb),
-                            col = "#8e8a8a") +
-         ggplot2::facet_wrap(~ pb, ncol = ncol, scales = "free",
-                             labeller = ggplot2::as_labeller(ylabel),
-                             strip.position = "left") +
-         ggplot2::geom_label(data = data_tibble,
-                             mapping = ggplot2::aes(x = -Inf, y = Inf,
-                                                    label = pb),
-                             hjust = 0.09, vjust = 0.8, size = 3.5,
-                             label.padding = ggplot2::unit(0.75, "lines")
-                             ) +
-         ggplot2::theme_classic(base_line_size = 0.25, base_rect_size = 0.25) +
-         ggplot2::theme(axis.title.x = ggplot2::element_blank()) +
-         ggplot2::ylab("") +
-         ggplot2::theme(strip.background = ggplot2::element_blank(),
-               strip.placement = "outside") +
-         ggplot2::theme(panel.border = ggplot2::element_rect(colour = "#6b6767",
-                                                    fill = NA, size = 0.5))
+
+  # plot background
+  plot <- ggplot2::ggplot() +
+    ggpattern::geom_rect_pattern(data = df_bg,
+                                 ggplot2::aes(xmin = min_years,
+                                              xmax = max_years,
+                                              ymin = holocene,
+                                              ymax = pb_thresh),
+                                 pattern = "gradient",
+                                 pattern_fill = green,
+                                 pattern_fill2 = darkgreen,
+                                 alpha = 0.8) +
+    ggpattern::geom_rect_pattern(data = df_bg,
+                                 ggplot2::aes(xmin = min_years,
+                                              xmax = max_years,
+                                              ymin = pb_thresh,
+                                              ymax = highrisk_thresh),
+                                 pattern = "gradient",
+                                 pattern_fill = yellow,
+                                 pattern_fill2 = red,
+                                 alpha = 0.8)  +
+    ggpattern::geom_rect_pattern(data = df_bg,
+                                 ggplot2::aes(xmin = min_years
+                                              xmax = max_years,
+                                              ymin = highrisk_thresh,
+                                              ymax = highrisk_thresh + (highrisk_thresh - pb_thresh) * 1.5), #nolint
+                                 pattern = "gradient",
+                                 p tern_fill = red,
+                                 p tern_fill2 = purple,
+                                 a ha = 0.8)  +
+    ggpattern::geom_rect_pattern(data = df_bg,
+                                 ggplot2::aes(xmin = min_years,
+                                              xmax = max_years,
+                                              ymin = highrisk_thresh + (highrisk_thresh - pb_thresh) * 1.5), #nolint
+                                              ymax = Inf,
+                                 pattern = "gradient",
+                                 pattern_fill = purple,
+                                 pattern_fill2 = purple,
+                                 alpha = 0.8)
+   #ggplot2::coord_cartesian(ylim = c(0, pb_thresh + 3 * (holocene - pb_thresh)))
+  
+  # plot pb status timeseries
+  #TODO the Y axis shouls be flexible depending on the PB
+  plot <- plot +
+    ggplot2::geom_ribbon(data = data_tibble, ggplot2::aes(x = years,
+                         ymin = values, ymax = Inf), fill = "white") +
+     ggplot2::geom_hline(data = df_bg,
+                         mapping = ggplot2::aes(yintercept = pb_thresh),
+                         col = darkgreen) +
+     ggplot2::geom_hline(data = df_bg,
+                         mapping = ggplot2::aes(yintercept = highrisk_thresh),
+                         col = lightred, linewidth = 0.25) +
+     ggplot2::geom_line(data = data_tibble,
+                        ggplot2::aes(x = years, y = values, group = pb),
+                        col = "gray31") +
+     ggplot2::facet_wrap(~ pb, ncol = ncol, scales = "free",
+                         labeller = ggplot2::as_labeller(ylabel),
+                         strip.position = "left") +
+     ggplot2::geom_label(data = data_tibble,
+                         mapping = ggplot2::aes(x = -Inf, y = Inf,
+                                                label = pb),
+                         hjust = 0.09, vjust = 0.8, size = 3.5,
+                         label.padding = ggplot2::unit(0.75, "lines")
+                         ) +
+     ggplot2::theme_classic(base_line_size = 0.25, base_rect_size = 0.25) +
+     ggplot2::theme(axis.title.x = ggplot2::element_blank()) +
+     ggplot2::ylab("") +
+     ggplot2::theme(strip.background = ggplot2::element_blank(),
+           strip.placement = "outside") +
+     ggplot2::theme(panel.border = ggplot2::element_rect(colour = "#6b6767",
+                                                fill = NA, size = 0.5)) +
+     ggplot2::scale_y_continuous(expand = c(0, NA))
 
   }
   print(plot)
